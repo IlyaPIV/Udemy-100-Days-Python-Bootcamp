cec2f6274921cecd025c6a7117c0b359b4177032
Ilya <petermoloch@gmail.com>
2023-06-24 19:41:30 +0300

    Day 8

---
+++ Day 8 - Beginner/main.py
@@ -0,0 +1,16 @@
+# def greed(ending):
+#     print("Hello")
+#     print("World")
+#     print(ending)
+#
+# greed("Bitch")
+
+# Functions with more that 1 input
+def greet_with(name, surname):
+    print(f"Hello, {name}!")
+    print(f"Your surname is {surname}?")
+
+greet_with("Jack", "Daniels")
+
+greet_with(name= "Justin", surname="Beeber")
+greet_with(surname="Beeber", name= "Justin")
